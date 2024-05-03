778e4e422191b248afc13d491700496a92f62519
mattv993 <matt.vinall7@gmail.com>
2024-05-03 19:17:25 -0400

    added underline for minutes for light theme

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,7 @@ export const Hero = () => {
     return (
         <section className={`flex flex-col items-center justify-center border-b-8 h-[50vh] dark:border-white border-black`}>
             <h1 className="text-6xl md:text-8xl dark:text-white mb-12 font-display text-center">Welcome to SkillScout</h1>
-            <p className=" text-2xl md:text-3xl dark:text-white text-center">Helping developers find in-demand skills in <span className="dark:border-white dark:border-b-4">minutes</span>, not hours.</p>
+            <p className=" text-2xl md:text-3xl dark:text-white text-center">Helping developers find in-demand skills in <span className="dark:border-white border-black border-b-4">minutes</span>, not hours.</p>
         </section>
     )
 }

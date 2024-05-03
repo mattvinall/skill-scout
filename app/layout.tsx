4ffcbcb5c6c4aa3e39e2ed00c4f3c45a4595ed4a
Matt Vinall <matt.vinall7@gmail.com>
2024-05-03 11:48:04 -0400

    added Rammetto_One font  as fontDisplay with weight 400

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,22 @@
 import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google";
+import { Inter as FontSans, Rammetto_One as FontDisplay } from "next/font/google";  // Renamed for clarity
 import "./globals.css";
 import { cn } from "@/lib/utils"
 import { ThemeProvider } from "@/components/theme-provider"
 import { Header } from "@/components/Header";
+import { Hero } from "@/components/Hero";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
-})
+});
+
+// Configuration for 'Rammetto One' font
+const fontDisplay = FontDisplay({
+  subsets: ["latin"],  // Adjust subsets as needed
+  variable: "--font-display",  // Use a different variable for this font
+  weight: "400",  // Adjust weight as needed
+});
 
 export const metadata: Metadata = {
   title: "Skill Scout",
@@ -24,7 +32,8 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={cn(
         "min-h-screen bg-background font-sans antialiased",
-        fontSans.variable
+        fontSans.variable,
+        fontDisplay.variable,
       )}>
         <ThemeProvider
           attribute="class"
@@ -33,6 +42,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <Header />
+          <Hero />
           {children}
         </ThemeProvider>
       </body>

a2a48a4b9e27a67e6d3a0a35083593aaed4d8e5d
Matt Vinall <matt.vinall7@gmail.com>
2024-05-03 11:48:29 -0400

    Hero section component

---
+++ components/Hero.tsx
@@ -0,0 +1,8 @@
+export const Hero = () => {
+    return (
+        <section className={`flex flex-col items-center justify-center border-b-8 h-[50vh] dark:border-white border-black`}>
+            <h1 className="text-8xl dark:text-white mb-12 font-display">Welcome to SkillScout</h1>
+            <p className="text-3xl dark:text-white">Helping developers find in demand skills in minutes, not hours.</p>
+        </section>
+    )
+}
